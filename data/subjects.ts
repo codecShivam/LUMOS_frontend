24986c3543f2cba0b909a3888050c3e84f13e321
codecShivam <codecshivam@gmail.com>
2023-11-12 09:59:48 +0530

    subjectList component

---
+++ data/subjects.ts
@@ -0,0 +1,32 @@
+export const subjects = [
+  {
+    title: "SENSOR & INSTRUMENTATION (BOE-305)",
+    description:
+      "This subject covers the basics of sensors and instrumentation used in various fields of engineering.",
+    image: "/images/sensor.jpg",
+  },
+  {
+    title: "UNIVERSAL HUMAN VALUES (BVE-301)",
+    description:
+      "This subject covers the importance of universal human values in personal and professional life.",
+    image: "/images/values.jpg",
+  },
+  {
+    title: "DISCRETE STRUCTURES & THEORY OF LOGIC (BCS-303)",
+    description:
+      "This subject covers the basics of discrete structures and the theory of logic used in computer science.",
+    image: "/images/discrete.jpg",
+  },
+  {
+    title: "DATA STRUCTURE (BCS-301)",
+    description:
+      "This subject covers the basics of data structures used in computer science.",
+    image: "/images/data.jpg",
+  },
+  {
+    title: "COMPUTER ORGANIZATION AND ARCHITECTURE (BCS-302)",
+    description:
+      "This subject covers the basics of computer organization and architecture used in computer science.",
+    image: "/images/architecture.jpg",
+  },
+];

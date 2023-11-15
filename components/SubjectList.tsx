abb8c2b1f2b0aeeb285ea50702132ebb17db353e
codecShivam <codecshivam@gmail.com>
2023-11-15 12:48:08 +0530

    added chatbot components

---
+++ components/SubjectList.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image"
+import { subjects } from "@/data/subjects"
+import { redirect } from "next/dist/server/api-utils"
+
+export const SubjectList = () => (
+
+    <div className="flex flex-col sm:flex-row flex-1">
+
+        <div className="bg-gray-100 w-full  p-4">
+            <h2 className="text-lg font-bold mb-4 text-gray-700">Subjects</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+                {subjects.map((subject) => (
+                    <div
+                        key={subject.title}
+                        className="bg-white rounded-md shadow-md p-4 flex flex-col justify-between"
+                    >
+                        <div>
+                            <Image
+                                src={subject.image}
+                                alt={subject.title}
+                                width={300}
+                                height={200}
+                                className="mb-4 w-full rounded-md "
+                                objectFit="cover"
+                            />
+                            <h3 className="text-gray-500 font-medium text-xl mb-2">
+                                {subject.title}
+                            </h3>
+                            <p className="text-gray-500 mb-4">{subject.description}</p>
+                        </div>
+                        <a href="/chat" className="bg-blue-500 text-center text-white py-2 px-4 rounded-md w-full">
+                            Ask Query
+                        </a>
+                    </div>
+                ))}
+            </div>
+        </div>
+    </div>
+)
